92c58dd9ad861eb12a2dffaa32f81c835018e3e1
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-08-17 20:01:05 -0700

    Add range info to CG data.

--- cg/core/src/main/java/com/github/tommyettinger/cg/ColorGuardData.java
+++ cg/core/src/main/java/com/github/tommyettinger/cg/ColorGuardData.java
@@ -13,18 +13,22 @@ public class ColorGuardData {
         public String name;
         public String primary;
         public String secondary;
+        public int primaryRange;
+        public int secondaryRange;
         public ObjectSet<String> places;
 
         public Unit(String name, String placement){
-            this(name, null, null, placement);
+            this(name, null, 0, null, 0, placement);
         }
-        public Unit(String name, String primary, String placement){
-            this(name, primary, null, placement);
+        public Unit(String name, String primary, int primaryRange, String placement){
+            this(name, primary, primaryRange, null, 0, placement);
         }
-        public Unit(String name, String primary, String secondary, String placement){
+        public Unit(String name, String primary, int primaryRange, String secondary, int secondaryRange, String placement){
             this.name = name;
             this.primary = primary;
             this.secondary = secondary;
+            this.primaryRange = primaryRange;
+            this.secondaryRange = secondaryRange;
             places = ObjectSet.with(placement.split(" "));
         }
 
@@ -34,39 +38,39 @@ public class ColorGuardData {
     }
 
     public static List<Unit> units = ObjectList.with(
-            new Unit("Infantry", "Machine_Gun", "Coast Desert Forest Ice Jungle Mountains Plains River Rocky Ruins"),
-            new Unit("Bazooka", "Handgun", "Forward_Missile", "Coast Desert Forest Ice Jungle Mountains Plains River Rocky Ruins"),
-            new Unit("Bike", "Machine_Gun", "Coast Desert Forest Ice Jungle Plains Ruins"),
-            new Unit("Rifle_Sniper", "Handgun", "Handgun", "Coast Desert Forest Ice Jungle Mountains Plains River Rocky Ruins"),
-            new Unit("Mortar_Sniper", "Arc_Cannon", "Coast Desert Forest Ice Jungle Mountains Plains River Rocky Ruins"),
-            new Unit("Missile_Sniper", "Arc_Missile", "Coast Desert Forest Ice Jungle Mountains Plains River Rocky Ruins"),
-            new Unit("Light_Tank", "Forward_Cannon", "Machine_Gun", "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
-            new Unit("War_Tank", "Forward_Cannon", "Machine_Gun", "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
-            new Unit("Scout_Tank", "Forward_Cannon", "Handgun", "Coast Desert Forest Ice Jungle Plains River Rocky Ruins"),
-            new Unit("Heavy_Cannon", "Forward_Cannon", "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
-            new Unit("Recon", "Machine_Gun", "Coast Desert Forest Ice Jungle Plains Ruins"),
-            new Unit("AA_Gun", "Machine_Gun", "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
-            new Unit("Flamethrower", "Flame_Wave", "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
-            new Unit("Light_Artillery", "Arc_Cannon", "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
-            new Unit("Rocket_Artillery", "Arc_Missile", "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
-            new Unit("AA_Artillery", "Arc_Missile", "Coast Desert Forest Ice Jungle Plains Ruins"),
+            new Unit("Infantry", "Machine_Gun", 1, "Coast Desert Forest Ice Jungle Mountains Plains River Rocky Ruins"),
+            new Unit("Bazooka", "Handgun", 1, "Forward_Missile", 1, "Coast Desert Forest Ice Jungle Mountains Plains River Rocky Ruins"),
+            new Unit("Bike", "Machine_Gun", 1, "Coast Desert Forest Ice Jungle Plains Ruins"),
+            new Unit("Rifle_Sniper", "Handgun", 1, "Handgun", 2, "Coast Desert Forest Ice Jungle Mountains Plains River Rocky Ruins"),
+            new Unit("Mortar_Sniper", "Arc_Cannon", 3, "Coast Desert Forest Ice Jungle Mountains Plains River Rocky Ruins"),
+            new Unit("Missile_Sniper", "Arc_Missile", 2, "Coast Desert Forest Ice Jungle Mountains Plains River Rocky Ruins"),
+            new Unit("Light_Tank", "Forward_Cannon", 1, "Machine_Gun", 1, "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
+            new Unit("War_Tank", "Forward_Cannon", 1, "Machine_Gun", 1, "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
+            new Unit("Scout_Tank", "Forward_Cannon", 1, "Handgun", 2, "Coast Desert Forest Ice Jungle Plains River Rocky Ruins"),
+            new Unit("Heavy_Cannon", "Forward_Cannon", 2, "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
+            new Unit("Recon", "Machine_Gun", 1, "Coast Desert Forest Ice Jungle Plains Ruins"),
+            new Unit("AA_Gun", "Machine_Gun", 1, "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
+            new Unit("Flamethrower", "Flame_Wave", 1, "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
+            new Unit("Light_Artillery", "Arc_Cannon", 2, "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
+            new Unit("Rocket_Artillery", "Arc_Missile", 3, "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
+            new Unit("AA_Artillery", "Arc_Missile", 4, "Coast Desert Forest Ice Jungle Plains Ruins"),
             new Unit("Supply_Truck", "Coast Desert Forest Ice Jungle Plains Ruins"),
             new Unit("Amphi_Transport", "Coast Desert Forest Ice Jungle Ocean Plains River Rocky Ruins"),
             new Unit("Build_Rig", "Coast Desert Forest Ice Jungle Plains Rocky Ruins"),
-            new Unit("Jammer", "Hack", "Coast Desert Forest Ice Jungle Plains Ruins"),
-            new Unit("Comm_Copter", "Hack", "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
-            new Unit("Jetpack", "Machine_Gun", "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
+            new Unit("Jammer", "Hack", 2, "Coast Desert Forest Ice Jungle Plains Ruins"),
+            new Unit("Comm_Copter", "Hack", 3, "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
+            new Unit("Jetpack", "Machine_Gun", 1, "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
             new Unit("Transport_Copter", "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
-            new Unit("Blitz_Copter", "Machine_Gun", "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
-            new Unit("Gunship_Copter", "Machine_Gun", "Forward_Missile", "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
-            new Unit("Patrol_Boat", "Machine_Gun", "Ocean River"),
-            new Unit("Battleship", "Arc_Cannon", "Ocean"),
-            new Unit("Cruiser", "Arc_Missile", "Torpedo", "Ocean River"),
-            new Unit("Submarine", "Arc_Missile", "Torpedo", "Ocean"),
-            new Unit("Legacy_Plane", "Machine_Gun", "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
-            new Unit("Fighter_Jet", "Forward_Missile", "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
-            new Unit("Stealth_Jet", "Forward_Missile", "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
-            new Unit("Heavy_Bomber", "Bomb_Drop", "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
+            new Unit("Blitz_Copter", "Machine_Gun", 1, "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
+            new Unit("Gunship_Copter", "Machine_Gun", 1, "Forward_Missile", 1, "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
+            new Unit("Patrol_Boat", "Machine_Gun", 2, "Ocean River"),
+            new Unit("Battleship", "Arc_Cannon", 3, "Ocean"),
+            new Unit("Cruiser", "Arc_Missile", 2, "Torpedo", 1, "Ocean River"),
+            new Unit("Submarine", "Arc_Missile", 3, "Torpedo", 1, "Ocean"),
+            new Unit("Legacy_Plane", "Machine_Gun", 1, "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
+            new Unit("Fighter_Jet", "Forward_Missile", 1, "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
+            new Unit("Stealth_Jet", "Forward_Missile", 1, "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
+            new Unit("Heavy_Bomber", "Bomb_Drop", 1, "Coast Desert Forest Ice Jungle Mountains Ocean Plains River Rocky Ruins"),
             new Unit("City", "Coast Desert Forest Ice Jungle Plains"),
             new Unit("Mansion", "Coast Desert Forest Ice Jungle Plains"),
             new Unit("Fort", "Coast Desert Forest Ice Jungle Plains"),
@@ -87,27 +91,27 @@ public class ColorGuardData {
         int r = IntPointHash.hashAll(BitConversion.floatToIntBits(x),
                 BitConversion.floatToIntBits(y), seed);
         Noise n = Noise.instance;
-        n.setNoiseType(Noise.FOAM_FRACTAL);
+        n.setNoiseType(Noise.SIMPLEX_FRACTAL);
         n.setSeed(seed);
-        n.setFrequency(0x5.83p-7f);
+        n.setFrequency(0x3.23p-7f);
         n.setFractalType(Noise.FBM);
         n.setFractalOctaves(4);
         float high = n.getConfiguredNoise(x, y, n.getConfiguredNoise(y, x));
         n.setSeed(seed ^ 0xC965815B);
-        n.setFrequency(0xE.09p-7f);
+        n.setFrequency(0x7.09p-7f);
         n.setFractalType(Noise.RIDGED_MULTI);
         n.setFractalOctaves(3);
         high = high * 0.5f + n.getConfiguredNoise(x, y, n.getConfiguredNoise(y, x)) * 0.5f;
         high = high / (((0.4f - 1f) * (1f - Math.abs(high))) + 1.0000001f);
-        n.setSeed(seed ^ 0xDE916ABC);//0xC965815B
-        n.setFrequency(0x8.13p-5f);
+        n.setSeed(seed ^ 0xDE916ABC);
+        n.setFrequency(0x6.13p-5f);
         n.setFractalType(Noise.FBM);
         n.setFractalOctaves(3);
         float hot = n.getConfiguredNoise(x, y, n.getConfiguredNoise(y, x));
         hot = hot / (((0.333f - 1f) * (1f - Math.abs(hot))) + 1.0000001f);
 
         n.setSeed(~seed);
-        n.setFrequency(0x5.13p-6f);
+        n.setFrequency(0x3.13p-6f);
         n.setFractalType(Noise.FBM);
         n.setFractalOctaves(2);
         float wet = n.getConfiguredNoise(x, y, n.getConfiguredNoise(y, x));
