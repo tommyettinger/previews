5cbca6b9ba9cf7a549fccad3ac4e1e147708272b
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-09-22 03:23:43 -0700

    GWT still isn't building; no idea why.
    This does update the palette, at least, so the Yellow Army has silver as its highlight color, rather than brown.

--- cg/lwjgl3/src/main/java/com/github/tommyettinger/cg/lwjgl3/Lwjgl3Launcher.java
+++ cg/lwjgl3/src/main/java/com/github/tommyettinger/cg/lwjgl3/Lwjgl3Launcher.java
@@ -2,7 +2,7 @@ package com.github.tommyettinger.cg.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.github.tommyettinger.cg.Main;
+import com.github.tommyettinger.cg.MainOld;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
@@ -11,7 +11,7 @@ public class Lwjgl3Launcher {
 	}
 
 	private static Lwjgl3Application createApplication() {
-		return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+		return new Lwjgl3Application(new MainOld(), getDefaultConfiguration());
 	}
 
 	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
