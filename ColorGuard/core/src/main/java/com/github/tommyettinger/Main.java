2aa6daa555b50f38e7e06672114fd30168f44975
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-01-28 20:03:07 -0800

    Update cg with new outlines.

--- ColorGuard/core/src/main/java/com/github/tommyettinger/Main.java
+++ ColorGuard/core/src/main/java/com/github/tommyettinger/Main.java
@@ -3,7 +3,6 @@ package com.github.tommyettinger;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
